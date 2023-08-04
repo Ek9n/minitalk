929ed0bc995509dbbfdf7084eb471a77387ccfb2
Hannes Stein <hstein@c4c4c2.42berlin.de>
2023-08-04 23:49:32 +0200

    push

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: hstein <hstein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 01:50:00 by hstein            #+#    #+#             */
-/*   Updated: 2023/08/02 03:14:25 by hstein           ###   ########.fr       */
+/*   Updated: 2023/08/04 21:27:08 by hstein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include "./libft/libft.h"
 #include "./printf/ft_printf.h"
--- src/client.c
+++ src/client.c
@@ -6,75 +6,90 @@
 /*   By: hstein <hstein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 02:27:10 by hstein            #+#    #+#             */
-/*   Updated: 2023/08/02 03:33:34 by hstein           ###   ########.fr       */
+/*   Updated: 2023/08/04 23:48:42 by hstein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-int				g_pid_server;
-uint32_t		*g_msg;
-const uint32_t	g_bitsize = 8;
+static bool	g_flag;
 
-void	send_char(int pid, uint32_t g_bitsize, char c)
+void	send_char(int pid, uint32_t bitsize, char c)
 {
 	uint32_t	bit;
 
 	bit = -1;
-	while (++bit < g_bitsize)
+	while (++bit < bitsize)
 	{
 		if (c >> bit & 1)
-		{
-			write(1, "1", 1);
 			kill(pid, SIGUSR1);
-		}
 		else
-		{
-			write(1, "0", 1);
 			kill(pid, SIGUSR2);
-		}
-		usleep(100);
+		usleep(140);
 	}
 }
 
-static void	send_g_msg(int pid, const uint32_t *g_msg)
+static void	send_msg(int pid, const uint32_t *msg, uint32_t bitsize)
 {
 	char		c;
-	int			g_bitsize_g_msg;
-	int			bytesize_g_msg;
-	char		*cast_g_msg;
+	int			bitsize_msg;
+	int			bytesize_msg;
+	char		*cast_msg;
 
-	g_bitsize_g_msg = ft_strlen((char *)g_msg) * g_bitsize;
-	bytesize_g_msg = ft_strlen((char *)g_msg);
-	cast_g_msg = (char *)g_msg;
-	printf("\nBits sent: %d\n", g_bitsize_g_msg);
-	c = *cast_g_msg;
-	while (*cast_g_msg++ && bytesize_g_msg)
+	bitsize_msg = ft_strlen((char *)msg) * bitsize;
+	bytesize_msg = ft_strlen((char *)msg);
+	cast_msg = (char *)msg;
+	printf("\nBits sent: %d\n", bitsize_msg);
+	c = *cast_msg;
+	while (*cast_msg++ && bytesize_msg)
 	{
-		send_char(pid, g_bitsize, c);
-		c = *cast_g_msg;
-		bytesize_g_msg--;
+		send_char(pid, bitsize, c);
+		c = *cast_msg;
+		bytesize_msg--;
+	}
+}
+
+static void	handler(int sig, siginfo_t *info, void *context)
+{
+	(void) info;
+	(void) context;
+	if (sig == SIGUSR2)
+	{
+		ft_printf("\n#MSG confirmed by server\n");
+		g_flag = true;
+	}
+	else
+	{
+		ft_printf("\n(handler) something happened\n");
+		exit(0);
 	}
 }
 
 int	main(int argc, char **argv)
 {
+	struct sigaction	s_sigaction;
+	int					pid_server;
+	uint32_t			*msg;
+	uint32_t			bitsize;
+
+	bitsize = 8;
 	if (argc == 3)
 	{
-		g_pid_server = atoi(argv[1]);
-		g_msg = (void *)argv[2];
-		write(1, "#send - g_msg:", ft_strlen("#send - g_msg:"));
-		write(1, argv[2], ft_strlen(argv[2]));
-		send_g_msg(g_pid_server, g_msg);
-		*g_msg = 4;
-		write(1, "\n#send - end of transmission (EOT)", \
-			ft_strlen("\n#send - end of transmission (EOT)"));
-		send_g_msg(g_pid_server, g_msg);
+		s_sigaction.sa_sigaction = handler;
+		s_sigaction.sa_flags = SA_SIGINFO;
+		pid_server = atoi(argv[1]);
+		msg = (void *)argv[2];
+		write(1, "#send - msg:", ft_strlen("#send - msg\n..."));
+		send_msg(pid_server, msg, bitsize);
+		ft_printf("\n#send - end of transmission (EOT)");
+		*msg = 4;
+		sigaction(SIGUSR2, &s_sigaction, NULL);
+		send_msg(pid_server, msg, bitsize);
+		while (!g_flag)
+		{
+		}
 	}
 	else
-	{
-		write(1, "-client error-\nno valid g_msg\n", \
-			ft_strlen("-client error-\nno valid g_msg\n"));
-	}
+		ft_printf("-client error-\nno valid msg\n");
 	return (0);
 }
